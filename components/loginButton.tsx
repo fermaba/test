0d6ec23958a48898fa2d593008d5a09ef47ca731
fermaba <131990122+fermaba@users.noreply.github.com>
2023-04-27 18:54:13 +0300

    Add files via upload

--- components/loginButton.tsx
+++ components/loginButton.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { Button } from '@mui/material';
 
 const LoginButton = () => {
-  const trelloLogin = `https://trello.com/1/authorize?response_type=token&key=${process.env.NEXT_PUBLIC_TRELLO_KEY}&return_url=http://localhost:3000/trello/auth&callback_method=fragment&scope=read,write&expiration=never&name=Logger`;
+  const trelloLogin = `https://trello.com/1/authorize?response_type=token&key=66196052e0874cd6053399639259887c&return_url=https://test-git-main-dshabelyan-gmailcom.vercel.app/trello/auth&callback_method=fragment&scope=read,write&expiration=never&name=Logger`;
 
   return (
     <Link href={trelloLogin}>
